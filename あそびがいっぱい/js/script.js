aba77e76f4f187b14198566df08fe4759df8d9cc
tankuuuumi <150252496+tankuuuumi@users.noreply.github.com>
2024-04-15 18:27:33 +0900

    あそびがいっぱい

--- あそびがいっぱい/js/script.js
+++ あそびがいっぱい/js/script.js
@@ -17,4 +17,13 @@ $(function () {
     return false;
   });
 
+  //カルーセル
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+  });
+
 });
